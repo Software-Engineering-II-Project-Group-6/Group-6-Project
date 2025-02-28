f8ec449227020fc32882f53a2d5d8d0f7e21a9a0
james123he <146311710+james123he@users.noreply.github.com>
2025-02-27 23:44:55 -0800

    Update AI_Chat.jsx

--- NourishQuest/public/AI_Chat.jsx
+++ NourishQuest/public/AI_Chat.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import NutritionAIChat from '../protected/components/NutritionAIChat';
 
 // Wait for DOM to be fully loaded
