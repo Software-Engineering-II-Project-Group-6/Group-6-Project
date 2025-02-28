247c01281c4a5f758ce3ae518da4311b172b8ca8
james123he <146311710+james123he@users.noreply.github.com>
2025-02-27 17:58:06 -0800

    Update AI_Routes.js

--- NourishQuest/routes/AI_Routes.js
+++ NourishQuest/routes/AI_Routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { processMessage, getUserContext } = require('../public/aiService');
+const { processMessage, getUserContext } = require('../public/AI_Service');
 
 // Middleware to check authentication
 const requireAuth = (req, res, next) => {
